a33036f3b01edae40ec84c75c1288c08094ec27c
Kim Jong Un <gobedsaseno@MacBook-Air-gobed.local>
2022-08-22 22:47:43 +0700

    Login endpoint

---
+++ handler/user.go
@@ -0,0 +1,80 @@
+package handler
+
+import (
+	"golangvueapp/helper"
+	"golangvueapp/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	//tangkap input dari user
+	//map input dari user ke struct RegisterUserInput
+	// struct di atas kita passing sebagai parameter service
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMesssage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMesssage)
+
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "tokentokentoken")
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+
+}
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMesssage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMesssage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMesssage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMesssage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokentokentokeen")
+	response := helper.APIResponse("Successsfully Loggedin", http.StatusOK, "error", formatter)
+	c.JSON(http.StatusOK, response)
+
+}
