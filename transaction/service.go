5af56b5e8741645626a17d272d26caca6330c298
kesatriakolorhitam123 <kesatriakolorhitam123@gmail.com>
2022-08-28 21:17:27 +0700

    get user's transaction endpoint

--- transaction/service.go
+++ transaction/service.go
@@ -12,6 +12,7 @@ type service struct {
 
 type Service interface {
 	GetTransactionByCampaignID(input GetCampaignTransactionInput) ([]Transaction, error)
+	GetTransactionsByUserID(userID int) ([]Transaction, error)
 }
 
 func NewService(repository Repository, campaignRepository campaign.Repository) *service {
@@ -35,4 +36,13 @@ func (s *service) GetTransactionByCampaignID(input GetCampaignTransactionInput) 
 	}
 
 	return transaction, nil
+}
+
+func (s *service) GetTransactionsByUserID(userID int) ([]Transaction, error) {
+	transactions, err := s.repository.GetByUserID(userID)
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
 }
